b5371e0f5ea36bb1e585fc23a9d86300e915ae49
David Björelind <david.bjorelind@gmail.com>
2021-06-18 12:02:01 +0200

    Updating ML-page with thesis

--- src/components/Machine_Learning/ML.js
+++ src/components/Machine_Learning/ML.js
@@ -4,6 +4,7 @@ import ReactTooltip from 'react-tooltip';
 import styled from 'styled-components';
 import './ML.css';
 import {kaggle_info} from './Kaggle-info'
+import thesis from '../../components/Media/thesis.pdf' 
 
 class ML extends React.Component {
 
@@ -11,11 +12,31 @@ class ML extends React.Component {
         return(
             <Wrapper>
                 <Kaggle>
-                    Check out my ML work on Kaggle!
+
+                    <Title>
+                        Check out my ML work on Kaggle!
+                    </Title>
+                    
                     <Ital> <a data-tip={kaggle_info.text}> What is Kaggle? </a> </Ital>
                     <ReactTooltip place="top" type="dark" effect="float" border="true" multiline="true"/>
 
                     <Link href="https://www.kaggle.com/dbjrelind2"> My profile on Kaggle</Link>
+                </Kaggle>
+
+                <Kaggle>
+                    
+                    <Title>
+                        Master's Thesis: Clustering and Summarization of Chat Dialogues
+                    </Title>
+                    
+
+                    <Text>
+                    My Master's Thesis was written within the area of NLP. Deep Contextualized Text Representations (S-BERT and FastText) in combination with clustering algorithms was used to extract problem areas within a data set of customer chat dialogues. It was shown that stable results were achieved when using more than 1,000 chats. A supervised approach would be interesting to investigate if more labeled data was available.
+                    </Text>
+        
+                    <ReactTooltip place="top" type="dark" effect="float" border="true" multiline="true"/>
+
+                    <Link href={thesis} target="_blank" > Read my thesis </Link>
                 </Kaggle>
 
             </Wrapper>
@@ -36,21 +57,21 @@ const Wrapper = styled.div`
     padding: 50px;
     padding-top: 10px;
 
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     display: flex;
     flex-direction: column;
 
-    min-height: 70vh; /* arbitrary height */
+    min-height: 75vh; /* arbitrary height */
 `
 const Kaggle = styled.div`
     background: rgb(0,0,100);
-    padding: 50px;
+    padding: 40px;
     border-style: solid;
     border-color: rgb(180, 180, 180);
     border-radius: 15px;
 
-    font-size: 18px;
+    font-size: 22px;
 
     display: flex;
     flex-direction: column;
@@ -65,6 +86,8 @@ const Link = styled.a`
     
     text-decoration: none;
 
+    font-size: 18px;
+
     &:hover{
         color: rgb(0, 150, 0);
         transition: 200ms ease-in;
@@ -76,3 +99,13 @@ const Ital = styled.b`
     color: rgb(180, 180, 180);
 `
     
+const Text = styled.b`
+    font-size: 18px;
+    text-align: center;
+    font-style: bold;
+    color: #F2E6D0;
+    padding-bottom: 20px;
+`
+const Title = styled.b`
+    padding-bottom: 20px;
+`
